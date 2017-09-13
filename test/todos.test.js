aa2d39315353cfb81729f3873ae22f772da25afb
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2017-09-13 19:44:14 -0300

    Adicionados testes com moch, chai e supertest.

---
+++ test/todos.test.js
@@ -0,0 +1,42 @@
+const app = require('../app');
+const request = require('supertest');
+const expect = require('chai').expect;
+
+describe('App', function () {
+    describe('/todos', function () {
+
+        it('GET /todos', function (done) {
+            request(app)
+                .get('/todos')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.body).to.be.a('array');
+                    done();
+                });
+        });
+
+        it('GET /todo/:id', function(done) {
+            request(app)
+                .get('/todos/1')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then((response) => {
+                    expect(response.body).to.be.a('object');
+                    expect(response.body).to.have.property('id');
+                    expect(response.body).to.have.property('title');
+                    expect(response.body).to.have.property('description');
+                    expect(response.body).to.have.property('completed');
+                    done();
+                });
+        });
+
+        it('DELETE /todo/:id', function(done) {
+            request(app)
+                .delete('/todos/1')
+                .expect(200, done);
+        });
+    });
+});
