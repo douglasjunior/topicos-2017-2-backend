a6b8a94989e471e1f8e15950990211e9a822ddfe
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2017-09-28 09:38:26 -0300

    Adicionado sequelize para conexão com banco de dados.

---
+++ models/index.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+const config = {
+    dialect: 'sqlite',
+    storage: './banco.sqlite',
+    define: {
+        // desativa o plural
+        freezeTableName: true,
+        // desativa colunas createdAt e updatedAt
+        timestamps: false
+    }
+};
+
+const sequelize = new Sequelize(null, null, null, config);
+
+const Todo = sequelize.define('todo', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+    },
+    title: Sequelize.STRING(50),
+    description: Sequelize.STRING(300),
+    completed: Sequelize.BOOLEAN,
+    creation_date: Sequelize.DATE,
+});
+
+module.exports = {
+    sequelize,
+    Todo,
+}
